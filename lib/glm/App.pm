8af9c8c3fce8613d0acf3cb8e426a4774e85c87f
tmoore82 <moore.tyler.n@gmail.com>
2016-05-25 03:59:09 +0100

    updated list to be able to add new items not already in db:

--- lib/glm/App.pm
+++ lib/glm/App.pm
@@ -16,8 +16,8 @@ get '/logout' => sub {
 
 };
 
-# get '/' => sub {
-get '/' => require_login sub {
+get '/' => sub {
+#get '/' => require_login sub {
 	
 	my $sth = database->prepare('SELECT name FROM product', { RaiseError => 1 });
 	$sth->execute();
@@ -28,20 +28,10 @@ get '/' => require_login sub {
 
 };
 
-# post '/my-list' => sub {
-post '/my-list' => require_login sub {
+post '/my-list' => sub {
+#post '/my-list' => require_login sub {
 
 	my $glist = param "glist";
-	#my @split_glist = split(/,/,$glist);
-
-	#foreach my $item (@split_glist) {
-	#	$item = '\'' . $item . '\'';
-	#}
-
-	#my $joined_glist = join(',',@split_glist);
-	# return $joined_glist;
-
-	#my $sth = database->prepare('SELECT store.name AS store, product.name As product, brand.name AS brand, category.name AS category, item.price AS price, item.size AS size, unit.name AS unit, item.ppu AS ppu FROM item LEFT JOIN location ON location.id = item.location LEFT JOIN store ON store.id = location.store LEFT JOIN brand ON brand.id = item.brand LEFT JOIN product ON product.id = item.product LEFT JOIN unit ON unit.id = item.unit LEFT JOIN category ON category.id = product.category WHERE location.id IN (1,2,3) AND product.name IN (' . $joined_glist . ') AND item.ppu = ( SELECT MIN(ppu) FROM item AS i2 WHERE i2.product = product.id) ORDER BY store.name, category.name, product.name', { RaiseError => 1 });
 
 	my $sth = database->prepare('SELECT * FROM get_items(\'{' . $glist . '}\')', { RaiseError => 1} );
 
